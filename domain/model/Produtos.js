3a478744ef39cdb20084ea30f318db6d95a9fded
Natália Sena <nataliasenasantos@gmail.com>
2024-05-03 18:36:36 -0300

    novos métodos

--- domain/model/Produtos.js
+++ domain/model/Produtos.js
@@ -12,19 +12,16 @@ class Produto {
 
     async adicionarProduto() {
         try {
-            //validação de esquema
             const { error } = schemaProduto.validate({ nome_produto: this.nome_produto, preco: this.preco, categoria_id: this.categoria_id, descricao: this.descricao, imagem: this.imagem });
             if (error) {
                 throw new Error(error.details[0].message);
             }
 
-            //Conferir se a categoria já existe
             const categoriaExiste = await verificarCategoriaExistente(this.categoria_id);
             if (!categoriaExiste) {
                 throw new Error('A categoria especificada não existe.');
             }
 
-            //Conferir se o produto já foi cadastrado
             const produtoCadastrado = await verificarProdutoCadastrado(this.nome_produto);
             if (produtoCadastrado) {
                 throw new Error('Este produto já foi cadastrado anteriormente.');
@@ -32,22 +29,31 @@ class Produto {
 
             const query = 'insert into produtos (nome_produto, preco, categoria_id, descricao, imagem) values ($1, $2, $3, $4, $5)';
             await pool.query(query, [this.nome_produto, this.preco, this.categoria_id, this.descricao, this.imagem]);
-            
+
         } catch (error) {
             throw new Error(error.message);
         }
-    }    
+    }
+    //método de edição
+    async editarProduto() {
+
+    }
+
+    //método de remoção
+    async excluirProduto() {
+
+    }
 }
 
 async function verificarCategoriaExistente(categoria_id) {
     try {
         const query = 'SELECT * FROM categorias WHERE id = $1';
         const result = await pool.query(query, [categoria_id]);
-        
+
         if (result.rows.length === 0) {
             return false;
         }
-        
+
         return true;
     } catch (error) {
         throw new Error('Erro ao verificar categoria existente: ' + error.message);
@@ -58,11 +64,11 @@ async function verificarProdutoCadastrado(nome_produto) {
     try {
         const query = 'SELECT * FROM produtos WHERE nome_produto = $1';
         const result = await pool.query(query, [nome_produto]);
-        
+
         if (result.rows.length === 0) {
             return false;
         }
-        
+
         return true;
     } catch (error) {
         throw new Error('Erro ao verificar produto cadastrado: ' + error.message);
