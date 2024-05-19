8f20d6cf183356f4c21804da852fef331dda6cd3
Natália Sena <nataliasenasantos@gmail.com>
2024-05-19 18:01:17 -0300

    ajustes adicionar pedido

--- domain/model/Pedido.js
+++ domain/model/Pedido.js
@@ -33,27 +33,31 @@ class Pedido {
         const user_id = process.env.USER_ID;
         const external_pos_id = process.env.EXTERNAL_POS_ID;
 
-        const dadosQrCode = new QrCode(detalhes_pedido).toJSON()
-
+        const dadosQrCode = new QrCode(detalhes_pedido);
+        const qrCodeJSON = dadosQrCode.toJSON();
         // Inserir pedido na tabela pedidos
 
         const queryPedido = 'INSERT INTO pedidos (cliente_id, preco_total, external_reference) VALUES ($1, $2, $3) RETURNING pedido_id';
-        const pedidoResult = await pool.query(queryPedido, [this.client_id, Math.round(dadosQrCode.total_amount * 100), dadosQrCode.external_reference]);
+        const pedidoResult = await pool.query(queryPedido, [this.client_id, qrCodeJSON.total_amount * 100, qrCodeJSON.external_reference]);
 
-        for (const pedido of this.pedidos) {
-            const pedido_id = pedidoResult.rows[0].pedido_id;
+        const pedido_id = pedidoResult.rows[0].pedido_id;
+
+        for (const detalhe_pedido of detalhes_pedido) {
+            const queryNomeProduto = 'SELECT nome_produto FROM produtos WHERE id = $1';
+            const nomeProdutoResult = await pool.query(queryNomeProduto, [detalhe_pedido.produto_id]);
+            const nome_produto = nomeProdutoResult.rows[0].nome_produto;
+            console.log(nome_produto);
 
             // Inserir detalhes do pedido na tabela pedido_detalhado
-            const queryDetalhesPedido = 'INSERT INTO pedido_detalhado (pedido_id, produto_id, preco, quantidade) VALUES ($1, $2, $3, $4)';
-            await pool.query(queryDetalhesPedido, [pedido_id, pedido.produto_id, pedido.preco, pedido.quantidade]);
+            const queryDetalhesPedido = 'INSERT INTO pedido_detalhado (pedido_id, nome_produto, preco, quantidade) VALUES ($1, $2, $3, $4)';
+            await pool.query(queryDetalhesPedido, [pedido_id, nome_produto, detalhe_pedido.preco, detalhe_pedido.quantidade]);
 
-            // Adicionar o pedido enviado à lista de pedidos enviados
-            this.pedidosEnviados.push(pedido);
+            // Adicionar o detalhe do pedido à lista de pedidos enviados
+            this.pedidosEnviados.push(detalhe_pedido);
         }
 
         const apiUrl = `instore/orders/qr/seller/collectors/${user_id}/pos/${external_pos_id}/qrs`;
-
-        const resposta = await MercadoPagoAPI.APIPost(apiUrl, dadosQrCode)
+        const resposta = await MercadoPagoAPI.APIPost(apiUrl, qrCodeJSON);
 
         return resposta;
     }
