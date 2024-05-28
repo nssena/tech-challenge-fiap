85e9df3889f8b15223fd014cac687efef8754bf2
Natália Sena <nataliasenasantos@gmail.com>
2024-05-28 14:43:43 -0300

    docker compose

--- src/infrastructure/persistence/Database.js
+++ src/infrastructure/persistence/Database.js
@@ -1,4 +1,4 @@
-const { Pool } = require('pg')
+const { Pool } = require('pg');
 require('dotenv').config();
 
 const pool = new Pool({
@@ -10,3 +10,10 @@ const pool = new Pool({
 })
 
 module.exports = pool
+
+console.log(process.env);
+
+console.log('DB_HOST:', process.env.DB_HOST);
+console.log('DB_USER:', process.env.DB_USER);
+console.log('DB_PASSWORD:', process.env.DB_PASSWORD);
+console.log('DB_NAME:', process.env.DB_NAME);
